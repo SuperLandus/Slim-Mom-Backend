eb97ad6582114efa22ecf650f003adc101a6a110
Heza GÖLCÜK <h.golcuk91@outlook.com>
2025-04-18 21:57:23 +0300

    swagger dosyaları kök dizinine ayarlandı

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,4 @@ import path from 'node:path';
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
-export const SWAGGER_PATH = path.join(
-  process.cwd(),
-  'src',
-  'docs',
-  'swagger.json',
-);
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
